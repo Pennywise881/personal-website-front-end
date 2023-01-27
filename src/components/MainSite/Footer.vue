71b6aade8832e3e7962bcf9e901e02bb0bba009a
Pennywise881 <zamannafi3@gmail.com>
2023-01-27 06:17:22 +0600

    fixed the issue with the footer. need to make some changes in the navbar next

--- src/components/MainSite/Footer.vue
+++ src/components/MainSite/Footer.vue
@@ -3,18 +3,14 @@
 
     </div>
     <footer class="bg-red-900 text-center text-white p-2 md:p-5">
-        <div class="hidden md:block">
-            <span class="text-sm">© 2023 <a href="#" class="hover:underline">zamansprojects™</a>. All Rights Reserved.
+        <!-- <div>
+            <span class="text-sm">
+                © 2023 <a href="#" class="hover:underline">zamansprojects™</a>. All Rights Reserved.
             </span>
-        </div>
-        <div class="md:hidden flex flex-col justify-center">
-            <div class="flex justify-center">
-                <img v-for="social in this.socials" width="20" :src="social.src" :alt="social.alt" class="mr-2 hover:cursor-pointer">
-            </div>
-            <div>
-                <span class="mt-2 text-xs">© 2023 <a href="#" class="hover:underline">zamansprojects™</a>. All Rights Reserved.</span>
-            </div>
-        </div>
+        </div> -->
+        <p class="text-sm">
+            © 2023 <a href="#" class="hover:underline">zamansprojects™</a>. All Rights Reserved.
+        </p>
     </footer>
 
 </template>
@@ -25,26 +21,7 @@
 export default{
     name: 'Footer',
     data (){
-        return{
-            socials:[
-                {
-                    alt: 'linkedin-logo',
-                    src: 'https://upload.wikimedia.org/wikipedia/commons/c/ce/Linkedin_circle.svg'
-                },
-                {
-                    alt: 'github-logo',
-                    src: 'https://upload.wikimedia.org/wikipedia/commons/a/ae/Github-desktop-logo-symbol.svg'
-                },
-                {
-                    alt: 'youtube-logo',
-                    src: 'https://upload.wikimedia.org/wikipedia/commons/5/52/YouTube_social_white_circle_%282017%29.svg'
-                },
-                {
-                    alt: 'twitter-logo',
-                    src: 'https://upload.wikimedia.org/wikipedia/commons/4/4f/Twitter-logo.svg'
-                },
-            ]
-        }
+        return{}
     }
 }
 </script>
