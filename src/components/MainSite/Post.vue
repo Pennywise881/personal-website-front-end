f14c3ef7163ace349212d62e0a1dee55cbb12564
Pennywise881 <zamannafi3@gmail.com>
2023-02-23 21:40:45 +0600

    posts can now handle external project links as well as routes

--- src/components/MainSite/Post.vue
+++ src/components/MainSite/Post.vue
@@ -5,7 +5,7 @@
         </div>
 
         <div v-for="post in this.posts" class="p-6 bg-white border-2 border-gray-200 rounded-lg shadow-md mb-5">
-            <router-link v-if="post.project_link" :to="{ name: post.project_link }">
+            <router-link v-if="post.project_route" :to="{ name: post.project_route }">
                 <div class="flex items-center">
                     <p class="text-xl md:text-2xl font-bold text-red-800 capitalize hover:underline mr-3">{{post.title}}</p>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
@@ -14,6 +14,15 @@
                     </svg>
                 </div>
             </router-link>
+            <a v-else-if="post.project_link" :href="post.project_link" target="_blank">
+                <div class="flex items-center">
+                    <p class="text-xl md:text-2xl font-bold text-red-800 capitalize hover:underline mr-3">{{post.title}}</p>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                        <path d="M12.232 4.232a2.5 2.5 0 013.536 3.536l-1.225 1.224a.75.75 0 001.061 1.06l1.224-1.224a4 4 0 00-5.656-5.656l-3 3a4 4 0 00.225 5.865.75.75 0 00.977-1.138 2.5 2.5 0 01-.142-3.667l3-3z" />
+                        <path d="M11.603 7.963a.75.75 0 00-.977 1.138 2.5 2.5 0 01.142 3.667l-3 3a2.5 2.5 0 01-3.536-3.536l1.225-1.224a.75.75 0 00-1.061-1.06l-1.224 1.224a4 4 0 105.656 5.656l3-3a4 4 0 00-.225-5.865z" />
+                    </svg>
+                </div>
+            </a>
             <h5 v-else class="text-xl md:text-2xl font-bold text-red-800 capitalize">{{post.title}}</h5>
             <p class="italic text-gray-500 mb-2">Published: {{this.getFormatedDate(post.publish_date)}}</p>
             <p :id="post.key+'-text'" class="text-sm md:text-base mb-3 font-normal text-gray-700 text-justify" v-html="post.text"></p>
@@ -39,24 +48,15 @@ export default{
     mounted(){
         this.getPosts();
     },
-    // watch:{
-    //     section: function makeAPICall() {
-    //         this.getPosts();
-    //     }
-    // },
-    // updated(){
-    //     this.addBulletPointsToLists();
-    // },
     methods:{
         getPosts()
         {   
-            // console.log('From Post.vue:', this.section);
             if (this.section === 'games')return;
 
             axios
             .get(`/api/v1/home/get-posts/${this.section}`)
             .then((response) =>{
-                console.log(response.data);
+                // console.log(response.data);
                 this.posts = [];
                 this.posts = response.data
             })
