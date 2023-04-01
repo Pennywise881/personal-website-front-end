337f96335de96092aa951e1f3cd579355440a001
Pennywise881 <zamannafi3@gmail.com>
2023-04-01 15:46:18 +0600

    removed all the post files that acted as individual components

--- src/main.js
+++ src/main.js
@@ -8,8 +8,8 @@ import 'flowbite';
 // import {v4 as uuidv4} from 'uuid';
 
 
-axios.defaults.baseURL = 'https://myboringproject.com';
-// axios.defaults.baseURL = 'http://127.0.0.1:8000'; 
+// axios.defaults.baseURL = 'https://myboringproject.com';
+axios.defaults.baseURL = 'http://127.0.0.1:8000'; 
 // axios.defaults.withCredentials = true
 
 createApp(App).use(store).use(router, axios).mount('#app')
