f5d7865e505243c1b9382dfdba9f1c28ac9cf12b
Pennywise881 <zamannafi3@gmail.com>
2023-05-18 02:45:12 +0600

    added links to socials

--- src/components/MainSite/SideBar.vue
+++ src/components/MainSite/SideBar.vue
@@ -1,5 +1,6 @@
 <template>
-    <aside class="hidden md:flex flex-col justify-between w-1/6 border-r-2 border-gray-200 h-screen" :class="{ 'fixed': !this.isShowNavbar}">
+    <aside class="hidden md:flex flex-col justify-between w-1/6 border-r-2 border-gray-200 h-screen"
+        :class="{ 'fixed': !this.isShowNavbar }">
         <!-- <div class="p-4 flex justify-between" :class="{'hidden': this.showNavbar}">
             <div class="font-bold cursor-pointer hover:underline text-red-800 uppercase">blog</div>.
             <div class="font-bold cursor-pointer hover:underline text-red-800 uppercase">ai-ml</div>.
@@ -10,7 +11,8 @@
         <div class="py-4 px-3 opacity-75 hover:opacity-100">
             <ul class="mt-5">
                 <li>
-                    <img class="m-auto rounded-full border-4 border-red-600 shadow-sm w-32" src="../../../src/assets/img.png" alt="user image" />
+                    <img class="m-auto rounded-full border-4 border-red-600 shadow-sm w-32"
+                        src="../../../src/assets/img.png" alt="user image" />
                 </li>
                 <li>
                     <p class="my-3 text-center font-bold text-xl">Nafi Uz Zaman</p>
@@ -22,12 +24,14 @@
             <div class="p-5 my-3 flex flex-col">
                 <div v-for="social in this.socials" class="flex mb-3">
                     <img width="25" :alt="social.alt" :src="social.src">
-                    <p class="ml-2 hover:text-red-800 hover:underline hover:cursor-pointer">{{social.title}}</p>
+                    <!-- <p class="ml-2 hover:text-red-800 hover:underline hover:cursor-pointer">{{ social.title }}</p> -->
+                    <a class="ml-2" :href="social.link" target="_blank">{{ social.title }}</a>
                 </div>
             </div>
         </div>
 
-        <p class="text-xs mt-auto p-1 text-center">© 2023 <a href="#" class="hover:underline">zamansprojects™</a>. All Rights Reserved.</p>
+        <p class="text-xs mt-auto p-1 text-center">© 2023 <a href="#" class="hover:underline">zamansprojects™</a>. All
+            Rights Reserved.</p>
     </aside>
 </template>
 
@@ -35,40 +39,43 @@
 import { mapState } from 'vuex';
 
 
-export default{
+export default {
     name: 'SideBar',
-    computed:{
+    computed: {
         ...mapState(['showNavbar']),
     },
-    watch:{
-        showNavbar(val)
-        {
+    watch: {
+        showNavbar(val) {
             this.isShowNavbar = val;
         }
     },
-    data(){
-        return{
+    data() {
+        return {
             isShowNavbar: null,
-            socials:[
+            socials: [
                 {
                     title: 'LinkedIn',
                     alt: 'linkedin-logo',
-                    src: 'https://upload.wikimedia.org/wikipedia/commons/c/ce/Linkedin_circle.svg'
+                    src: 'https://upload.wikimedia.org/wikipedia/commons/c/ce/Linkedin_circle.svg',
+                    link: "https://www.linkedin.com/in/nafi-uz-zaman-684523222/"
                 },
                 {
                     title: 'GitHub',
                     alt: 'github-logo',
-                    src: 'https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg'
+                    src: 'https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg',
+                    link: "https://github.com/NafiZaman"
                 },
                 {
                     title: 'YouTube',
                     alt: 'youtube-logo',
-                    src: 'https://upload.wikimedia.org/wikipedia/commons/5/52/YouTube_social_white_circle_%282017%29.svg'
+                    src: 'https://upload.wikimedia.org/wikipedia/commons/5/52/YouTube_social_white_circle_%282017%29.svg',
+                    link: "https://www.youtube.com/channel/UCQN52RZJuLUk6mXGGgFwbaA"
                 },
                 {
                     title: 'Twitter',
                     alt: 'twitter-logo',
-                    src: 'https://upload.wikimedia.org/wikipedia/commons/6/6f/Logo_of_Twitter.svg'
+                    src: 'https://upload.wikimedia.org/wikipedia/commons/6/6f/Logo_of_Twitter.svg',
+                    link: "#"
                 },
             ]
         }
--- src/components/MainSite/Social.vue
+++ src/components/MainSite/Social.vue
@@ -1,62 +1,67 @@
 <template>
     <div class="md:hidden pt-28 pb-10 px-5 flex w-full">
-        <img class="rounded-full border-2 border-red-600 shadow-sm w-16" src="../../../src/assets/img.png" alt="user image" />
+        <img class="rounded-full border-2 border-red-600 shadow-sm w-16" src="../../../src/assets/img.png"
+            alt="user image" />
         <div class="flex flex-col justify-center">
             <p class="mx-2 font-bold">Nafi Uz Zaman</p>
             <p class="mx-2 text-sm font-light">NLP, Web & Gamedev</p>
         </div>
         <div class="flex items-center">
-            <button @click="this.showSocials()" class="ml-10 rounded hover:bg-black hover:text-white text-sm p-1 bg-transparent border border-black">
+            <button @click="this.showSocials()"
+                class="ml-10 rounded hover:bg-black hover:text-white text-sm p-1 bg-transparent border border-black">
                 Follow
                 <div class="font-normal absolute shadow-md mt-2 right-20 mr-5 hidden socials-menu">
                     <div class="bg-zinc-200 border-4 border-white px-2 rounded">
-                        <div v-for="social in this.socials" class="flex px-1 py-2">
+                        <div v-for="social in  this.socials " class="flex px-1 py-2">
                             <img width="16" :alt="social.alt" :src="social.src">
-                            <p class="ml-2 hover:text-red-800 hover:underline hover:cursor-pointer text-black">{{social.title}}</p>
+                            <!-- <p class="ml-2 hover:text-red-800 hover:underline hover:cursor-pointer text-black">{{social.title}}</p> -->
+                            <a class="ml-2 text-black" :href="social.link" target="_blank">{{ social.title }}</a>
                         </div>
                     </div>
                 </div>
             </button>
         </div>
     </div>
-
 </template>
 
 
 <script>
 
-export default{
+export default {
     name: 'Social',
-    data (){
-        return{
-            socials:[
+    data() {
+        return {
+            socials: [
                 {
                     title: 'LinkedIn',
                     alt: 'linkedin-logo',
-                    src: 'https://upload.wikimedia.org/wikipedia/commons/c/ce/Linkedin_circle.svg'
+                    src: 'https://upload.wikimedia.org/wikipedia/commons/c/ce/Linkedin_circle.svg',
+                    link: "https://www.linkedin.com/in/nafi-uz-zaman-684523222/"
                 },
                 {
                     title: 'GitHub',
                     alt: 'github-logo',
-                    src: 'https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg'
+                    src: 'https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg',
+                    link: "https://github.com/NafiZaman"
                 },
                 {
                     title: 'YouTube',
                     alt: 'youtube-logo',
-                    src: 'https://upload.wikimedia.org/wikipedia/commons/5/52/YouTube_social_white_circle_%282017%29.svg'
+                    src: 'https://upload.wikimedia.org/wikipedia/commons/5/52/YouTube_social_white_circle_%282017%29.svg',
+                    link: "https://www.youtube.com/channel/UCQN52RZJuLUk6mXGGgFwbaA"
                 },
                 {
                     title: 'Twitter',
                     alt: 'twitter-logo',
-                    src: 'https://upload.wikimedia.org/wikipedia/commons/6/6f/Logo_of_Twitter.svg'
+                    src: 'https://upload.wikimedia.org/wikipedia/commons/6/6f/Logo_of_Twitter.svg',
+                    link: "#"
                 },
             ]
         }
     },
-    methods:{
-        showSocials()
-        {
-          document.querySelector('.socials-menu').classList.toggle('hidden');
+    methods: {
+        showSocials() {
+            document.querySelector('.socials-menu').classList.toggle('hidden');
         }
     }
 }
